a72791faa71f135b7e9fce1ba62452768b8bae5c
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-06-08 22:39:31 +0900

    update link process

--- dotlink.sh
+++ dotlink.sh
@@ -1,25 +1,51 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -ue
 
-set -u
+helpmsg() {
+    echo "Usage: $0 [--help | -h]" 0>&2
+    echo ""
+}
 
-DOT_DIR=$(cd $(dirname $0); pwd)
-
-cd ${DOT_DIR}
-git submodule init
-git submodule update
-
-echo "Setup dot links..."
-for f in .??*
-do
-    [[ "$f" = ".DS_Store" ]] && continue
-    [[ "$f" = ".git" ]] && continue
-    [[ "$f" = ".gitignore" ]] && continue
-    [[ "$f" = ".gitmodules" ]] && continue
-    if [[ -f $f ]]; then
-        ln -snfv ${DOT_DIR}/${f} ${HOME}/${f}
-    elif [[ -d $f ]]; then
-        rm -rf ${HOME}/${f}
-        ln -snfv ${DOT_DIR}/${f} ${HOME}/${f}
+link_to_homedir() {
+    if [ ! -d "$HOME/.dotbackup" ];then
+        echo "$HOME/.dotbackup not found. Auto make it."
+        mkdir "$HOME/.dotbackup"
     fi
+
+    dotfiles_dir=$(cd $(dirname $0); pwd)
+    for f in $dotfiles_dir/.??*; do
+        local f_filename
+        f_filename=$(basename "$f")
+        [[ "$f_filename" == ".DS_Store" || \
+            "$f_filename" == ".git" || \
+            "$f_filename" == ".gitignore" || \
+            "$f_filename" == ".gitmodules" ]] && continue
+        # すでにシンボリックリンクが存在する場合は削除
+        if [[ -L "$HOME/$f_filename" ]];then
+            command rm -f "$HOME/$f_filename"
+        fi
+        # dotファイルまたはディレクトリが存在する場合はbackup
+        if [[ -e "$HOME/$f_filename" ]];then
+            command mv "$HOME/$f_filename" "$HOME/.dotbackup"
+        fi
+        command ln -snfv $f $HOME
+    done
+}
+
+while [ $# -gt 0 ];do
+    case ${1} in
+        --debug|-d)
+            set -uex
+            ;;
+        --help|-h)
+            helpmsg
+            exit 1
+            ;;
+        *)
+            ;;
+    esac
+    shift
 done
-echo "Deploy dotfiles complete!."
+
+link_to_homedir
+command echo -e "Install completed!!! \e[m"
